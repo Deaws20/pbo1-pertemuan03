1ffce4ae0b64d2bef31a05b4991eb6ac7b467ff0
Deaws20 <d.salsabyla@gmail.com>
2020-04-12 08:49:24 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah commentkomentar yang tidak dijalankan oleh program
+
+    String nama = "Dea Welan Salsabyla";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.5;
+}
